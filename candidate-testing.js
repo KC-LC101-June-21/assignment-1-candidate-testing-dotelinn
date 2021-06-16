94a13b4b5df08e3b80b2423fbb6b35e38844d2c1
Joel Dotel <84645726+dotelinn@users.noreply.github.com>
2021-06-16 16:03:12 +0000

    Completed part 2 and working on part 3

--- candidate-testing.js
+++ candidate-testing.js
@@ -8,9 +8,21 @@ let candidateName = "";
 let question = "Who was the first American woman in space? ";
 let correctAnswer = "Sally Ride";
 let candidateAnswer = "";
-let questions;
-let correctAnswers;
-let candidateAnswers;
+let questions = [
+  '1) Who was the first American woman in space?',
+  '2)True or false: 5 kilometer == 5000 meters?',
+  '3) (5 + 3)/2 * 10 = ?',
+  "4) Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2?",
+  '5) What is the minimum crew size for the ISS?',
+];
+let correctAnswers = [
+  "Sally Ride",
+  "true",
+  "40",
+  "Trajectory",
+  "3",
+];
+let candidateAnswers = [ ];
 
 
 function askForName() {
@@ -21,21 +33,36 @@ function askForName() {
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-candidateAnswer = input.question(question);
 
+  for (i = 0; i < questions.length; i++) {
+  
+  candidateAnswers.push(input.question(questions[i]));
+  
+  console.log(`Your Answer: ${candidateAnswers[i]}\nCorrect Anwser: ${correctAnswers[i]}\n`);
+  }
 }
 
 function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-  if (candidateAnswer === correctAnswer) {
-    console.log('Correct')
-   } else {
-    console.log('Incorrect')
-   }
-
-  let grade;
   
+  let grade = 0;
+
+  for (i = 0; i < questions.length; i++) {
+     if (candidateAnswers[i].toLowercase === correctAnswers[i].toLowercase) {
+       grade += 1;
+     } else {
+       ;
+     }
+}
+  
+  if (grade >= 4) {
+    console.log(`>>> Overall Grade: ${grade}% (${grade} of 5 responses correct) <<<
+>>> Status: PASS <<<`)
+  } else {
+    console.log(`>>> Overall Grade: ${grade}% (${grade} of 5 responses correct) <<<
+>>> Status: FAILED <<<`)
+  }
 
   return grade;
 }
